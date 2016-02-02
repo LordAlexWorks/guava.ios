b8eda7c7c83e802fc22fa0962e5890e687ba6d5f
Susim <susim.samanta@me.com>
2016-02-02 10:26:03 +0530

    Create basic project structures. Group files & folders

---
+++ Guava/Model/Login.swift
@@ -0,0 +1,13 @@
+//
+//  Login.swift
+//  Guava
+//
+//  Created by Susim Samanta on 02/02/16.
+//  Copyright © 2016 LordAlexWorks. All rights reserved.
+//
+
+import UIKit
+
+class Login: NSObject {
+
+}
