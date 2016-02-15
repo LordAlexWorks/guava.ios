7e6107c329375234bca7e1f9b21f130dc7be1dc7
Susim <susim.samanta@me.com>
2016-02-15 10:18:23 +0530

    basic auto layout #10

--- Goyava/Controller/ViewController/MainContentVC.swift
+++ Goyava/Controller/ViewController/MainContentVC.swift
@@ -10,13 +10,19 @@ import UIKit
 
 class MainContentVC: UIViewController {
     var pageIndex : Int = 0
+    @IBOutlet weak var bottomConstraint : NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.updateBottomConstraint()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func updateBottomConstraint(){
+        print(self.view.frame.size.height)
+        self.bottomConstraint.constant = self.view.frame.size.height - 470.0 
     }
 }
