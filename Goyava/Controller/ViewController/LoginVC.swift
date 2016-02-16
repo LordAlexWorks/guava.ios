dc38a02a512e99e271ba0d463e7181ac87e5d23a
Susim <susim.samanta@me.com>
2016-02-16 08:21:27 +0530

    #5 & #6

--- Goyava/Controller/ViewController/LoginVC.swift
+++ Goyava/Controller/ViewController/LoginVC.swift
@@ -8,53 +8,31 @@
 
 import UIKit
 
-class LoginVC: UIViewController, SignInTableViewDelegate {
-
-     var signDelegate: SignInTableViewDelegate?
-    
-    @IBOutlet weak var loginButtonPress: UIButton!
-    @IBOutlet weak var facebookButtonPress: UIButton!
-    @IBOutlet weak var twitterButtonPress: UIButton!
+class LoginVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.view.backgroundColor = UIColor.blackColor()
-//        signDelegate = self
-//        addBackgroundTapGesture()
-        
-//        self.loginButtonPress.layer.cornerRadius = 27
-//        self.loginButtonPress.layer.borderWidth = 2
-//        self.loginButtonPress.layer.borderColor = UIColor.whiteColor().CGColor
-//        
-//        self.facebookButtonPress.layer.cornerRadius = 28
-//        self.facebookButtonPress.layer.borderColor = UIColor.facebookColor().CGColor
-//        self.facebookButtonPress.backgroundColor = UIColor.facebookColor()
-//        self.facebookButtonPress.layer.borderWidth = 2
-//        
-//        self.twitterButtonPress.layer.cornerRadius = 28
-//        self.twitterButtonPress.layer.borderColor = UIColor.twitterColor().CGColor
-//        self.twitterButtonPress.backgroundColor = UIColor.twitterColor()
-//        self.twitterButtonPress.layer.borderWidth = 2
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-    //MARK: SignInTableViewDelegate
-    func signInButtonTapped(sender: UIButton) {
-        performSegueWithIdentifier("signInToSingUpSegue", sender: nil)
+    //MARK: Button Action
+    @IBAction func signUpButtonTapped(sender: UIButton) {
+        let signupVc = self.storyboard?.instantiateViewControllerWithIdentifier("SignUpVC") as! SignUpVC
+        signupVc.modalTransitionStyle = .FlipHorizontal
+        self.presentViewController(signupVc, animated: true) { () -> Void in
+        }
     }
-
-    //MARK: Login navigation
-    @IBAction func loginButtonTapped(sender: UIButton) {                #if (arch(i386) || arch(x86_64)) && os(iOS)
+    @IBAction func loginButtonTapped(sender: UIButton) {
+        #if (arch(i386) || arch(x86_64)) && os(iOS)
             goToMyCard()
         #else
             goToScannerView()
         #endif
     }
-
+    //MARK: Login navigation
     func goToScannerView(){
         let qrscannerVc = self.storyboard?.instantiateViewControllerWithIdentifier("QRScannerVC") as! QRScannerVC
         self.presentViewController(qrscannerVc, animated: true) { () -> Void in
@@ -66,15 +44,13 @@ class LoginVC: UIViewController, SignInTableViewDelegate {
         appDelegate.window?.rootViewController = mainVc
     }
     
-    
-    
     //MARK: Background Tap
-//    func addBackgroundTapGesture(){
-//        let gestureRecognizer = UITapGestureRecognizer(target: self, action: "handleTap:")
-////        self.tableView.addGestureRecognizer(gestureRecognizer)
-//    }
-//    func handleTap(gestureRecognizer: UIGestureRecognizer) {
-//        self.view.endEditing(true)
-//    }
+    func addBackgroundTapGesture(){
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: "handleTap:")
+        self.view.addGestureRecognizer(gestureRecognizer)
+    }
+    func handleTap(gestureRecognizer: UIGestureRecognizer) {
+        self.view.endEditing(true)
+    }
     
 }
--- Goyava/Controller/ViewController/SignUpVC.swift
+++ Goyava/Controller/ViewController/SignUpVC.swift
@@ -8,32 +8,26 @@
 
 import UIKit
 
-class SignUpVC: UIViewController, SignInTableViewDelegate {
-
-    var signDelegate: SignInTableViewDelegate?
+class SignUpVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        signDelegate = self
-        
         addBackgroundTapGesture()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    //MARK: SignInTableViewDelegate
-    func signInButtonTapped(sender: UIButton) {
+    //MARK: Button Actions
+    @IBAction func loginButtonTapped(sender: UIButton) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
-    //MARK: Login navigation
-    @IBAction func loginButtonPress(sender: UIButton) {
+    
+    @IBAction func createAccountButtonTapped(sender: UIButton) {
         goToScannerView()
     }
-    
+    //MARK: Login navigation
     func goToScannerView(){
         let qrscannerVc = self.storyboard?.instantiateViewControllerWithIdentifier("QRScannerVC") as! QRScannerVC
         self.presentViewController(qrscannerVc, animated: true) { () -> Void in
@@ -43,7 +37,7 @@ class SignUpVC: UIViewController, SignInTableViewDelegate {
     //MARK: Background Tap
     func addBackgroundTapGesture(){
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: "handleTap:")
-//        self.tableView.addGestureRecognizer(gestureRecognizer)
+        self.view.addGestureRecognizer(gestureRecognizer)
     }
     func handleTap(gestureRecognizer: UIGestureRecognizer) {
         self.view.endEditing(true)
