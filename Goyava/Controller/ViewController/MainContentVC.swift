338df14520b65623225046d751eb9dc88cf17dd3
Susim <susim.samanta@me.com>
2016-03-04 18:22:01 +0530

    line width parameter added #41

--- Goyava/Controller/ViewController/MainContentVC.swift
+++ Goyava/Controller/ViewController/MainContentVC.swift
@@ -8,16 +8,24 @@
 
 import UIKit
 
+
 class MainContentVC: UIViewController {
     var pageIndex : Int = 0
     @IBOutlet weak var bottomConstraint : NSLayoutConstraint!
+    @IBOutlet weak var mondayProgressView : UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.showProgress()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func showProgress(){
+        CircleView.drawCircleOn(mondayProgressView, color: UIColor.grayColor(), progress:2,lineWidth: 2.0)
+        CircleView.drawCircleOn(mondayProgressView, color: UIColor.redColor(), progress:1,lineWidth : 2.0)
     }
     
 }
