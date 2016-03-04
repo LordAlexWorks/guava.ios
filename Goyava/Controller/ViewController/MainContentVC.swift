f6b9f3f6fdcc2f09a0542a7e6b9a69b997bedaca
Susim <susim.samanta@me.com>
2016-03-04 21:29:15 +0530

    weekly progress control added

--- Goyava/Controller/ViewController/MainContentVC.swift
+++ Goyava/Controller/ViewController/MainContentVC.swift
@@ -13,19 +13,31 @@ class MainContentVC: UIViewController {
     var pageIndex : Int = 0
     @IBOutlet weak var bottomConstraint : NSLayoutConstraint!
     @IBOutlet weak var mondayProgressView : UIView!
+    @IBOutlet weak var tuesdayProgressView : UIView!
+    @IBOutlet weak var wednesdayProgressView : UIView!
+    @IBOutlet weak var thrursdayProgressView : UIView!
+    @IBOutlet weak var fridayProgressView : UIView!
+    @IBOutlet weak var saturdayProgressView : UIView!
+    @IBOutlet weak var sundayProgressView : UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.showProgress()
+        self.showProgress(self.mondayProgressView)
+        self.showProgress(self.tuesdayProgressView)
+        self.showProgress(self.wednesdayProgressView)
+        self.showProgress(self.thrursdayProgressView)
+        self.showProgress(self.fridayProgressView)
+        self.showProgress(self.saturdayProgressView)
+        self.showProgress(self.sundayProgressView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    func showProgress(){
-        CircleView.drawCircleOn(mondayProgressView, color: UIColor.grayColor(), progress:2,lineWidth: 1.5,fillColor: UIColor.clearColor())
-        CircleView.drawCircleOn(mondayProgressView, color: UIColor.redColor(), progress:1,lineWidth : 1.5,fillColor: UIColor.clearColor())
+    func showProgress(progressView : UIView){
+        CircleView.drawCircleOn(progressView, color: UIColor.grayColor(), progress:2,lineWidth: 1.5,fillColor: UIColor.clearColor())
+        CircleView.drawCircleOn(progressView, color: UIColor(hexString:"29ABA9"), progress:1,lineWidth : 1.5,fillColor: UIColor.clearColor())
     }
     
 }
