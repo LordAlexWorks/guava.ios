8d1663c34e8a290782212e8218b38d1dea41800e
Susim <susim.samanta@me.com>
2016-03-07 07:41:32 +0530

    Services class and networking component added #42

---
+++ Goyava/Controller/Services/AppServices.swift
@@ -0,0 +1,13 @@
+//
+//  AppServices.swift
+//  Goyava
+//
+//  Created by Susim Samanta on 07/03/16.
+//  Copyright © 2016 LordAlexWorks. All rights reserved.
+//
+
+import UIKit
+
+class AppServices: NSObject {
+
+}
---
+++ Goyava/Main/AppConstants.swift
@@ -0,0 +1,13 @@
+//
+//  AppConstants.swift
+//  Goyava
+//
+//  Created by Susim Samanta on 07/03/16.
+//  Copyright © 2016 LordAlexWorks. All rights reserved.
+//
+
+import UIKit
+
+class AppConstants: NSObject {
+
+}
