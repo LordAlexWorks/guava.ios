36cf2ca16b9926e5576f3f6c94e70842e2a14ff8
Susim <susim.samanta@me.com>
2016-04-04 08:36:01 +0530

    domain classes added

---
+++ Goyava/Controller/Domain/CardsController.swift
@@ -0,0 +1,13 @@
+//
+//  CardsController.swift
+//  Goyava
+//
+//  Created by Susim Samanta on 04/04/16.
+//  Copyright © 2016 LordAlexWorks. All rights reserved.
+//
+
+import UIKit
+
+class CardsController: NSObject {
+
+}
