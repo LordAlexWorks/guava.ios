44eec87d470d0e0872b7b4be33a5855357daba02
Susim <susim.samanta@me.com>
2016-04-04 09:30:24 +0530

    create card model

---
+++ Goyava/Model/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Goyava
+//
+//  Created by Susim Samanta on 04/04/16.
+//  Copyright © 2016 LordAlexWorks. All rights reserved.
+//
+
+import UIKit
+
+class Card: NSObject {
+    var cardId : Int?
+    var maxPoint : Int?
+    var point : Int?
+    
+}
