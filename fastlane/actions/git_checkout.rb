f572edf956adc905a09cbe2db038e0a06ab117cf
bolo michelin <bolo@lordalexworks.com>
2016-04-12 18:22:06 -0400

    update Pod

--- fastlane/actions/git_checkout.rb
+++ fastlane/actions/git_checkout.rb
@@ -3,33 +3,33 @@ module Fastlane
         module SharedValues
             GIT_CHECKOUT_BRANCH = :GIT_CHECKOUT_BRANCH
         end
-        
+
         class GitCheckoutAction < Action
             def self.run(params)
             remote_branch = params[:remote_branch]
-            
+
             # construct our command as an array of components
             command = [
             'git',
             'checkout',
             remote_branch
             ]
-            
+
             # execute our command
             Actions.sh('pwd')
-            
+
             Actions.sh(command.join(' '))
             Helper.log.info "Successfully checkout branch #{remote_branch}."
         end
-        
+
         #####################################################
         # @!group Documentation
         #####################################################
-        
+
         def self.description
         'Checkout a branch from project working copy'
     end
-    
+
     def self.available_options
     [
     FastlaneCore::ConfigItem.new(key: :remote_branch,
@@ -47,7 +47,7 @@ def self.output
 end
 
 def self.authors
-["fabiomilano"]
+["bolom"]
 end
 
 def self.is_supported?(platform)
