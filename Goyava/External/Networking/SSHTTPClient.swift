ad55118954de9ba5474678eb744422108dbe62e5
bolo michelin <bolo@lordalexworks.com>
2016-07-12 18:01:38 -0400

    add target Goyava-Beta

--- Goyava/External/Networking/SSHTTPClient.swift
+++ Goyava/External/Networking/SSHTTPClient.swift
@@ -14,25 +14,28 @@ public class SSHTTPClient : NSObject {
     var httpMethod,urlString,httpBody: NSString?
     var headerFieldsAndValues : NSDictionary?
     
-    public init(url:String?, method:String?, httpBody: NSString?, headerFieldsAndValues: NSDictionary) {
+    //Initializer Method with url string , method, body , header field and values
+    public init(url:String?, method:String?, httpBody: NSString?, headerFieldsAndValues: NSDictionary?) {
         self.urlString =  url
         self.httpMethod = method
         if httpBody != nil {
             self.httpBody = httpBody!
         }
-        self.headerFieldsAndValues = headerFieldsAndValues
+        if headerFieldsAndValues != nil {
+            self.headerFieldsAndValues = headerFieldsAndValues!
+        }
+        
     }
-    
+    //Get formatted JSON
     public func getJsonData(_ httpResponseHandler : SSHTTPResponseHandler) {
         if self.urlString != nil {
-            let request = NSMutableURLRequest(url: Foundation.URL(string:self.urlString! as String)!)
+            var request = URLRequest(url: URL(string:self.urlString! as String)!)
             request.httpMethod =  self.httpMethod! as String
             self.headerFieldsAndValues?.enumerateKeysAndObjects({ (key, value, stop) -> Void in
                 request.setValue(value as! NSString as String, forHTTPHeaderField: key as! NSString as String)
             })
             request.httpBody = self.httpBody?.data(using: String.Encoding.utf8.rawValue)
-            let session = URLSession.shared()
-            
+            let session = URLSession.shared
             let task = session.dataTask(with: request, completionHandler: { (data, response , error) -> Void in
                 if (error == nil) {
                     var jsonError : NSError?
@@ -61,27 +64,29 @@ public class SSHTTPClient : NSObject {
             httpResponseHandler(obj: nil, error: nil)
         }
     }
-    public func getResponseData(_ urlString :NSString?,httpResponseHandler : SSHTTPResponseHandler) {
-        let request = NSMutableURLRequest(url: Foundation.URL(string:self.urlString! as String)!)
+    //Get Response in Data format
+    public func getResponseData(_ httpResponseHandler : SSHTTPResponseHandler) {
+        var request = URLRequest(url: URL(rawValue:self.urlString! as String)!)
         request.httpMethod =  self.httpMethod! as String
         self.headerFieldsAndValues?.enumerateKeysAndObjects({ (key, value, stop) -> Void in
             request.setValue(value as? String, forHTTPHeaderField: key as! NSString as String)
         })
         request.httpBody = self.httpBody?.data(using: String.Encoding.utf8.rawValue)
-        let session = URLSession.shared()
+        let session = URLSession.shared
         let task = session.dataTask(with: request, completionHandler: { (data, response , error) -> Void in
             if (error == nil) {
-               httpResponseHandler (obj: data, error: nil)
+                httpResponseHandler (obj: data, error: nil)
             }else {
-               httpResponseHandler(obj: nil,error: error)
+                httpResponseHandler(obj: nil,error: error)
             }
         })
         task.resume()
     }
     
+    //Cancel Request
     public func cancelRequest()->Void{
-        let session = URLSession.shared()
-		session.invalidateAndCancel()
+        let session = URLSession.shared
+        session().invalidateAndCancel()
     }
-
+    
 }
--- Podfile
+++ Podfile
@@ -11,6 +11,16 @@ target 'Goyava' do
     pod 'Reachability'
 end
 
+target 'Goyava-Beta' do
+    pod 'Fabric'
+    pod 'Crashlytics'
+    pod 'AWSMobileAnalytics'
+    pod 'LookbackSafe', :configurations => ['Release']
+    pod 'Realm', git: 'git@github.com:realm/realm-cocoa.git', branch: 'master'
+    pod 'RealmSwift', git: 'git@github.com:realm/realm-cocoa.git', branch: 'master'
+    pod 'Reachability'
+end
+
 post_install do |installer|
   installer.pods_project.targets.each do |target|
     target.build_configurations.each do |config|
